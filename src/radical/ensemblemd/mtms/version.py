4243d3d3dca49d0600b3448c8364f96a68a1ce9f
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-03-26 16:44:23 +0100

    Expose version.

---
+++ src/radical/ensemblemd/mtms/version.py
@@ -0,0 +1,6 @@
+__copyright__ = "Copyright 2014, http://radical.rutgers.edu"
+__license__   = "MIT"
+
+import os
+
+version = open(os.path.dirname (os.path.abspath (__file__)) + "/VERSION", 'r').read().strip()
